1e68f0d749a83d6fb337b68bb9b5e9ca28d44325
Abshalom Christopher <jabshalomchris@gmail.com>
2024-09-10 19:52:18 +0530

    Added Calendar demo project

---
+++ Calendars/Child.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendars
+{
+    public class Child
+    {   
+        private string _name;
+        private Date _birthDate;
+
+        public Child(string name, Date birthDate)
+        {
+            _name = name;
+            _birthDate = birthDate;
+        }
+    }
+}
---
+++ Calendars/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using Calendars;
+
+Date birthDate = new Date(2016, 2, 29);
+Child schoolChild = new Child("Sam", birthDate);
