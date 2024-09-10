6c39674f4e2043c2ebaed5f671a8fb204f19f888
Abshalom Christopher <jabshalomchris@gmail.com>
2024-09-10 19:50:29 +0530

    Added Child and Date classes

--- Calendars/Program.cs
+++ Calendars/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Calendars;
+
+Date birthDate = new Date(2016, 2, 29);
+Child schoolChild = new Child("Sam", birthDate);
