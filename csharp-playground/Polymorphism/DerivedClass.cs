bed056e62143ff8e5dd465bdcec9723b3464d67b
Abshalom Christopher <AChristopher@braveenergy.com.au>
2024-09-10 16:30:06 +0530

    Added Polymorphic classes

---
+++ csharp-playground/Polymorphism/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_playground.Polymorphism
+{
+    public class DerivedClass : ParentClass
+    {
+        public new void DoWork()
+        {
+            Console.WriteLine("Invoked from Derived");
+        }
+    }
+}
