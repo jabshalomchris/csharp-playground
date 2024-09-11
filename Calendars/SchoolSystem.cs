3f44f13a1c1bb691fda97e325a26878a487a7d3d
Abshalom Christopher <jabshalomchris@gmail.com>
2024-09-11 21:49:18 +0530

    Remove redundant code

---
+++ Calendars/SchoolSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendars
+{
+    public class SchoolSystem
+    {
+        private YearDate _cutoff;
+        private int _minAge;
+        private YearDate _schoolStart;
+
+        public SchoolSystem(YearDate cutOff, int minAge, YearDate schoolStart)
+        {
+            _cutoff = cutOff;
+            _minAge = minAge;
+            _schoolStart = schoolStart;
+        }
+    }
+}
---
+++ Calendars/YearDate.cs
@@ -0,0 +1,13 @@
+﻿namespace Calendars
+{
+    public class YearDate
+    {
+        private int _month;
+        private int _day;
+
+        public YearDate(int year, int month, int day)
+        {
+            
+        }
+    }
+}
