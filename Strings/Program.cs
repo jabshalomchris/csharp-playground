577b2a5459e7676cd143a7d76acd18147f461b8a
Abshalom Christopher <jabshalomchris@gmail.com>
2024-09-18 19:09:42 +0530

    Added Extensions Demo

---
+++ Strings/Program.cs
@@ -0,0 +1,11 @@
+﻿using Strings;
+
+string str1 = "Judah";
+string str2 = "Abshalom";
+
+Console.WriteLine(str1.Equals(str2));
+
+// Extension
+
+string str3 = "myextensiondemo";
+Console.WriteLine(str3.Capitalize());
---
+++ Strings/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    public static class StringExtension
+    {
+        public static string Capitalize(this string word)
+        {
+            return char.ToUpper(word[0]) + word.Substring(1);
+        }
+    }
+}
