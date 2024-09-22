3fc6b674d164103edd9cea7051b049296e3d2204
Abshalom Christopher <jabshalomchris@gmail.com>
2024-09-22 12:05:46 +0530

    LINQ Select many concept

--- csharp-playground/Program.cs
+++ csharp-playground/Program.cs
@@ -1,32 +1,32 @@
 ﻿using csharp_playground.Polymorphism;
 
 
-//// Specify the data source.
-//List<int> scores = [100, 97, 92, 81, 60];
+// Specify the data source.
+List<int> scores = [100, 97, 92, 81, 60];
 
-//scores.Sort();
+scores.Sort();
 
-//var x  = from score in scores where score > 80 select score;
+var x = from score in scores where score > 80 select score;
 
-//// Define the query expression.
-//IEnumerable<string> scoreQuery =
-//    from score in scores
-//    where score > 80
-//    orderby score ascending
-//    select $"Score {score}";
+// Define the query expression.
+IEnumerable<string> scoreQuery =
+    from score in scores
+    where score > 80
+    orderby score ascending
+    select $"Score {score}";
 
-//// Execute the query.
-//foreach (var i in scoreQuery)
-//{
-//    Console.WriteLine(i + " ");
-//}
+// Execute the query.
+foreach (var i in scoreQuery)
+{
+    Console.WriteLine(i + " ");
+}
 
-//// Output: 97 92 81
+// Output: 97 92 81
 
 
-//// Object substitution class
-//ParentClass cl = new DerivedClass();
-//cl.DoWork();
+// Object substitution class
+ParentClass cl = new DerivedClass();
+cl.DoWork();
 
 // LINQ Practices
 var listOfNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 6 };
@@ -42,3 +42,24 @@ var numbers = new List<int>() { 1, 2, 3 };
 var letters = new List<char>() { 'a', 'b', 'c' };
 
 var result = numbers.Zip(letters, (num,letter) => $"{num}-{letter}");
+
+
+// Aggregate
+
+
+// Select Many
+
+var orders = new List<Order>()
+{
+    new Order() {Id=1, Products = new List<string>{"Rice", "Cereal"} },
+    new Order() {Id=2, Products = new List<string>{"Cola"} }
+};
+
+var allProducts = orders.SelectMany(order => order.Products);
+
+
+class Order
+{
+    public int Id { get; set; }
+    public List<string> Products { get; set; }
+}
